f696aa271bb8abd0597dbf6d8202f21759ba6a1b
Leonardo Wellausen <lwellausen@gmail.com>
2017-07-03 01:10:22 -0300

    nothing special here

--- src/main.cpp
+++ src/main.cpp
@@ -164,9 +164,12 @@ float g_CameraTheta = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
 float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
 float g_CameraDistance = 3.5f; // Distância da câmera para a origem
 
-glm::vec4 camera_position_c  = glm::vec4(0.0f,5.0f,3.0f,1.0f);
-glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,-1.0f,0.0f);    //vetores tem que ser definiidos globalmente para serem acessados na função de callback
+//vetores tem que ser definiidos globalmente para serem acessados na função de callback
 glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f);
+glm::vec4 ship_position      = glm::vec4(1.0f,2.5f,0.0f,1.0f);
+glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,-1.0f,0.0f);
+glm::vec4 camera_position_c  = glm::vec4(0.0f,4.0f,-2.0f,0.0f);
+//glm::vec4 camera_position_c  = ship_position - camera_lookat_l;
 
 // Variáveis que controlam rotação do antebraço
 float g_ForearmAngleZ = 0.0f;
@@ -350,7 +353,7 @@ int main(int argc, char* argv[])
 
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slide 159 do documento "Aula_08_Sistemas_de_Coordenadas.pdf".
-        glm::vec4 camera_view_vector = camera_position_c - (camera_lookat_l+camera_position_c); // Vetor "view", sentido para onde a câmera está virada
+        glm::vec4 camera_view_vector = -camera_lookat_l; // Vetor "view", sentido para onde a câmera está virada
 
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
@@ -437,7 +440,8 @@ int main(int argc, char* argv[])
         glUniform1i(object_id_uniform, COW);
         DrawVirtualObject("cow");
 
-        model = Matrix_Translate(1.0f,2.5f,0.0f);
+        //ship
+        model = Matrix_Translate(ship_position.x, ship_position.y, ship_position.z);
         glUniformMatrix4fv(model_uniform, 1 , GL_FALSE , glm::value_ptr(model));
         glUniform1i(object_id_uniform, SHIP);
         DrawVirtualObject("ship");
